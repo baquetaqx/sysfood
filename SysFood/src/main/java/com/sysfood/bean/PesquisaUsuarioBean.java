53d671e37272bd4fca95710687e237ab45ef583a
MF & JA <MF & JA@192.168.1.4>
2017-02-23 20:11:19 -0300

    caixa

---
+++ SysFood/src/main/java/com/sysfood/bean/PesquisaUsuarioBean.java
@@ -0,0 +1,58 @@
+package com.sysfood.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.sysfood.bo.UsuarioBo;
+import com.sysfood.dao.filter.UsuarioFilter;
+import com.sysfood.model.Usuario;
+
+@Named
+@ViewScoped
+public class PesquisaUsuarioBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private List<Usuario> usuariosFiltrados;
+
+	@Inject
+	private UsuarioBo usuarioBo;
+
+	private UsuarioFilter filtro;
+	private Usuario usuario;
+
+	public PesquisaUsuarioBean() {
+		filtro = new UsuarioFilter();
+		usuario = new Usuario();
+
+	}
+
+	public void pesquisar() {
+		usuariosFiltrados = usuarioBo.filtrados(filtro);
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public List<Usuario> getUsuariosFiltrados() {
+		return usuariosFiltrados;
+	}
+
+	public void setUsuariosFiltrados(List<Usuario> usuariosFiltrados) {
+		this.usuariosFiltrados = usuariosFiltrados;
+	}
+
+	public UsuarioFilter getFiltro() {
+		return filtro;
+	}
+
+}
--- SysFood/src/main/java/com/sysfood/bo/UsuarioBo.java
+++ SysFood/src/main/java/com/sysfood/bo/UsuarioBo.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.inject.Inject;
 
 import com.sysfood.dao.UsuarioDao;
+import com.sysfood.dao.filter.UsuarioFilter;
 import com.sysfood.exception.NegocioException;
 import com.sysfood.model.Usuario;
 import com.sysfood.util.jpa.Transactional;
@@ -20,6 +21,10 @@ public class UsuarioBo implements Serializable{
 	@Transactional
 	public Usuario salvar(Usuario usuario) throws NegocioException {
 		return usuarioDao.guardar(usuario);
+	}
+
+	public List<Usuario> filtrados(UsuarioFilter filtro) {
+		return usuarioDao.filtrados(filtro);
 	}
 	
 }
