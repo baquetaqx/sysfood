f0a20590b0693469f592c74bbc9eea15412e6ac7
MF & JA <MF & JA@MFJA-PC>
2017-03-14 21:11:36 -0300

    Adicionado metodo para impressao controle de cozinha

--- SysFood/src/main/java/com/sysfood/bo/CumpomBo.java
+++ SysFood/src/main/java/com/sysfood/bo/CumpomBo.java
@@ -20,6 +20,7 @@ public class CumpomBo implements Serializable {
 		BematechNFiscal cupom = BematechNFiscal.Instance;
 		List<Integer> iRetornos = new ArrayList<>();
 
+		// Método para imprimir cupom para cliente
 		iRetornos.add(cupom.ConfiguraModeloImpressora(7));
 		iRetornos.add(cupom.IniciaPorta("COM3"));
 
@@ -47,6 +48,28 @@ public class CumpomBo implements Serializable {
 		iRetornos.add(cupom.FormataTX("---------------------------------------------\n", 2, 0, 0, 0, 0));
 
 		iRetornos.add(cupom.AcionaGuilhotina(1));
+
+		// Método para controle de cozinha
+		iRetornos.add(cupom.FormataTX("PASTEL DO CÉU\n\n", 3, 0, 0, 0, 0));
+		iRetornos.add(cupom.FormataTX("CONTROLE DA COZINHA\n", 2, 0, 0, 0, 0));
+		iRetornos.add(cupom.FormataTX("Nome do Cliente: " + pedido.getCliente().toUpperCase() + "\n", 2, 0, 0, 0, 0));
+		iRetornos.add(cupom.FormataTX(
+				"Data do pedido: " + new SimpleDateFormat("dd/MM/yyy - HH:mm").format(pedido.getDataPedido()) + "\n", 2,
+				0, 0, 0, 0));
+		iRetornos.add(cupom.FormataTX("=============================================\n", 2, 0, 0, 0, 0));
+		iRetornos.add(cupom.FormataTX("DESCRIÇÃO\n", 2, 0, 0, 0, 0));
+		iRetornos.add(cupom.FormataTX("=============================================\n", 2, 0, 0, 0, 0));
+
+		for (ItemPedido item : pedido.getItens()) {
+			iRetornos.add(cupom.FormataTX(
+					"Qtd = " + item.getQuantidade() + "/" + item.getProduto().getNome() + "\n" + "Adicionais: - " , 2, 0,
+					0, 0, 0));
+		}
+
+		iRetornos.add(cupom.FormataTX("=============================================\n", 2, 0, 0, 0, 0));
+		iRetornos.add(cupom.AcionaGuilhotina(1));
+
+		// Fechar Porta
 		iRetornos.add(cupom.FechaPorta());
 
 		for (Integer integer : iRetornos) {
@@ -56,7 +79,7 @@ public class CumpomBo implements Serializable {
 		}
 
 	}
-	
+
 	public void imprimirCaixa(Caixa caixa) throws NegocioException {
 		BematechNFiscal cupom = BematechNFiscal.Instance;
 		List<Integer> iRetornos = new ArrayList<>();
@@ -65,15 +88,17 @@ public class CumpomBo implements Serializable {
 
 		iRetornos.add(cupom.FormataTX("PASTEL DO CÉU - RELATÓRIO DE CAIXA\n\n\n", 3, 0, 0, 0, 0));
 		iRetornos.add(cupom.FormataTX("---------------------------------------------\n", 2, 0, 0, 0, 0));
-		iRetornos.add(cupom.FormataTX(
-				"Data de abertura:\t " + new SimpleDateFormat("dd/MM/yyy - HH:mm").format(caixa.getDataDeAbertura()) + "\n", 2,
-				0, 0, 0, 0));
-		iRetornos.add(cupom.FormataTX("Fundo de caixa:\t"+caixa.getFundoDeCaixa() +"\n", 2, 0, 0, 0, 0));
-		iRetornos.add(cupom.FormataTX("Débito:\t"+caixa.getDebito() +"\n", 2, 0, 0, 0, 0));
-		iRetornos.add(cupom.FormataTX("Crédito:\t"+caixa.getCredito() +"\n", 2, 0, 0, 0, 0));
-		iRetornos.add(cupom.FormataTX("Dinheiro:\t"+caixa.getDinheiro() +"\n", 2, 0, 0, 0, 0));
-		iRetornos.add(cupom.FormataTX("Número de pedidos:\t"+caixa.getNumeroDePedidos() +"\n", 2, 0, 0, 0, 0));
-		iRetornos.add(cupom.FormataTX("Total:\t"+caixa.getTotal() +"\n", 2, 0, 0, 0, 0));
+		iRetornos
+				.add(cupom.FormataTX(
+						"Data de abertura:\t "
+								+ new SimpleDateFormat("dd/MM/yyy - HH:mm").format(caixa.getDataDeAbertura()) + "\n",
+						2, 0, 0, 0, 0));
+		iRetornos.add(cupom.FormataTX("Fundo de caixa:\t" + caixa.getFundoDeCaixa() + "\n", 2, 0, 0, 0, 0));
+		iRetornos.add(cupom.FormataTX("Débito:\t" + caixa.getDebito() + "\n", 2, 0, 0, 0, 0));
+		iRetornos.add(cupom.FormataTX("Crédito:\t" + caixa.getCredito() + "\n", 2, 0, 0, 0, 0));
+		iRetornos.add(cupom.FormataTX("Dinheiro:\t" + caixa.getDinheiro() + "\n", 2, 0, 0, 0, 0));
+		iRetornos.add(cupom.FormataTX("Número de pedidos:\t" + caixa.getNumeroDePedidos() + "\n", 2, 0, 0, 0, 0));
+		iRetornos.add(cupom.FormataTX("Total:\t" + caixa.getTotal() + "\n", 2, 0, 0, 0, 0));
 		iRetornos.add(cupom.FormataTX("---------------------------------------------\n", 2, 0, 0, 0, 0));
 		for (Integer integer : iRetornos) {
 			if (integer == 0) {
